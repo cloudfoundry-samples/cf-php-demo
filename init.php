ce29677fdd9306938ebd6e6a85689f0438d5592d
Matt Stine <mstine@gopivotal.com>
2013-07-15 16:15:48 -0500

    improve readability of db connection string

--- init.php
+++ init.php
@@ -7,7 +7,16 @@ $db_url = $vcap_services->{'elephantsql-n/a'}[0]->credentials->uri;
 $db_params = parse_url($db_url);
 
 $db_name = substr($db_params['path'], 1);
-$db_conn = pg_connect("host=${db_params['host']} port=${db_params['port']} user=${db_params['user']} password=${db_params['pass']} dbname=${db_name}") or die('Could not connect: ' . pg_last_error());
+
+$connect_string = <<<CONNECT
+host=${db_params['host']}
+port=${db_params['port']}
+user=${db_params['user']}
+password=${db_params['pass']}
+dbname=${db_name}
+CONNECT;
+
+$db_conn = pg_connect($connect_string) or die('Could not connect: ' . pg_last_error());
 
 $query = 'SELECT id, language, url FROM buildpacks';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
